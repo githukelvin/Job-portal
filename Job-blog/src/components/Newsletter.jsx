8ae06fdca10d2c6429a81d80f7af1ae5dd4e7d93
Christine-design-web <wakuthiichristine5@gmail.com>
2024-03-01 11:53:45 +0300

    uploading CV section

--- Job-blog/src/components/Newsletter.jsx
+++ Job-blog/src/components/Newsletter.jsx
@@ -5,7 +5,7 @@ import Button from '../sidebar/Button'
 const Newsletter = () => {
   return (
     <div>
-      <div className="mt-20">
+      <div>
         <h3 className="text-lg font-bold mb-2 flex items-center gap-2">
             <FaEnvelopeOpenText/>
             Email me for Jobs</h3>
@@ -13,15 +13,15 @@ const Newsletter = () => {
             {/* subscription button */}
             <div>
                 <form className="w-full space-y-4">
-                  
-                    <input type="submit" value={"Upload your CV"} className="w-full block py-2 pl-3 border focus:outline-none bg-blue rounded-sm text-white cursor-pointer font-semibold hover:bg-pink"/>
+                    <input type="email" name="email" placeholder="name@gmail.com" className="w-full block py-2 pl-3 border focus:outline-none"/> 
+                    <input type="submit" value={"Subscribe"} className="w-full block py-2 pl-3 border focus:outline-none bg-blue rounded-sm text-white cursor-pointer font-semibold hover:bg-pink"/>
                 </form>
             </div>
             
       </div>
       {/* submission of the resume/cv */}
 
-      <div>
+      <div  className="mt-20">
         <h3 className="text-lg font-bold mb-2 flex items-center gap-2">
             <FaRocket/>
             Get Noticed faster
@@ -30,8 +30,8 @@ const Newsletter = () => {
             {/* subscription button */}
             <div>
                 <form className="w-full space-y-4">
-                    <input type="email" name="email" placeholder="name@gmail.com" className="w-full block py-2 pl-3 border focus:outline-none"/>
-                    <input type="submit" value={"Subscribe"} className="w-full block py-2 pl-3 border focus:outline-none bg-blue rounded-sm text-white cursor-pointer font-semibold hover:bg-pink"/>
+                   
+                    <input type="submit" value={"Upload your CV"} className="w-full block py-2 pl-3 border focus:outline-none bg-blue rounded-sm text-white cursor-pointer font-semibold hover:bg-pink"/>
                 </form>
             </div>
             
