032264dcfe757ba556e4122b32a27eaff379189a
Christine-design-web <wakuthiichristine5@gmail.com>
2024-03-01 11:49:50 +0300

    uploadingg the cv

--- Job-blog/src/components/Newsletter.jsx
+++ Job-blog/src/components/Newsletter.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import{ FaEnvelopeOpenText } from "react-icons/fa6"
+import{ FaEnvelopeOpenText, FaRocket } from "react-icons/fa6"
 import Button from '../sidebar/Button'
 
 const Newsletter = () => {
   return (
     <div>
-      <div>
+      <div className="mt-20">
         <h3 className="text-lg font-bold mb-2 flex items-center gap-2">
             <FaEnvelopeOpenText/>
             Email me for Jobs</h3>
@@ -13,8 +13,8 @@ const Newsletter = () => {
             {/* subscription button */}
             <div>
                 <form className="w-full space-y-4">
-                    <input type="email" name="email" placeholder="name@gmail.com" className="w-full block py-2 pl-3 border focus:outline-none"/>
-                    <input type="submit" value={"Subscribe"} className="w-full block py-2 pl-3 border focus:outline-none bg-blue rounded-sm text-white cursor-pointer font-semibold hover:bg-pink"/>
+                  
+                    <input type="submit" value={"Upload your CV"} className="w-full block py-2 pl-3 border focus:outline-none bg-blue rounded-sm text-white cursor-pointer font-semibold hover:bg-pink"/>
                 </form>
             </div>
             
@@ -23,8 +23,9 @@ const Newsletter = () => {
 
       <div>
         <h3 className="text-lg font-bold mb-2 flex items-center gap-2">
-            <FaEnvelopeOpenText/>
-            Email me for Jobs</h3>
+            <FaRocket/>
+            Get Noticed faster
+            </h3>
             <p className="text-primary/75 text-base mb-5">Join our newsletter and get the latest job listings and career insights delivered straight to your inbox</p>
             {/* subscription button */}
             <div>
