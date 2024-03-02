07eb461a45fe31acc2eb55e05767e007419826ff
Christine-design-web <wakuthiichristine5@gmail.com>
2024-03-02 13:36:23 +0300

    testing the job blog

--- Job-blog/src/Pages/Createjob.jsx
+++ Job-blog/src/Pages/Createjob.jsx
@@ -17,12 +17,12 @@ const Createjob = () => {
         {/* form */}
         <div className="bg-[#FAFAFA] py-10 px-4 lg:px-16">
         <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
+      
       <input defaultValue="test" {...register("example")} />
 
-      {/* include validation with required or other standard HTML validation rules */}
+     
       <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
+      
       {errors.exampleRequired && <span>This field is required</span>}
 
       <input type="submit" />
