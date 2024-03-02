46d04d2d2e9e8f00eee634ace4342e482d4211dc
Christine-design-web <wakuthiichristine5@gmail.com>
2024-03-02 17:51:01 +0300

    experience level

--- Job-blog/src/Pages/Createjob.jsx
+++ Job-blog/src/Pages/Createjob.jsx
@@ -18,7 +18,7 @@ const Createjob = () => {
     <div className="max-w-screen-2xl  container mx-auto xl:px-24 px-4">
         {/* form */}
         <div className="bg-[#FAFAFA] py-10 px-4 lg:px-16">
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5" >
           {/* 1st row */}
           <div className="create-job-flex">
             <div className="lg:w-1/2 w-full">
@@ -54,7 +54,56 @@ const Createjob = () => {
             </div>
 
           </div>
-          <input type="submit" />
+
+          {/* 3rd row */}
+          <div className="create-job-flex">
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">
+                Salary Type
+              </label>
+              <select {...register("salaryType")} className="create-job-input">
+        <option value="">Choose your salary</option>
+        <option value="Hourly">Hourly</option>
+        <option value="Monthly">Monthly</option>
+        <option value="Yearly">Yearly</option>
+      </select>
+            </div>
+            {/* company name */}
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">
+                Job Location
+              </label>
+               <input type="text" placeholder="Ex: Kenya" {...register("jobLocation")}  className="create-job-input"/>
+            </div>
+
+          </div>
+
+          {/* 4th row */}
+          <div className="create-job-flex">
+
+          <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">
+                Job Posting Date
+              </label>
+               <input type="date" placeholder="Ex: 2024-01-05" {...register("postingDate")}  className="create-job-input"/>
+            </div>
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">
+                Experience Level
+              </label>
+              <select {...register("salaryType")} className="create-job-input">
+        <option value="">Choose your Experience</option>
+        <option value="No experience">Any experience</option>
+        <option value="Internship">Internship</option>
+        <option value="Work remotely">Work remotely</option>
+      </select>
+            </div>
+            {/* company name */}
+            
+
+          </div>
+
+          <input type="submit" className=" block mt-12 bg-blue text-white hover:bg-pink font-semibold px-8 py-2 rounded-sm cursor-pointer" />
          </form>
         </div>
     </div>
