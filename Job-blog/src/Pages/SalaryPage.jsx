ab468403528aaacd2351d16467c834864c3eb92d
Christine-design-web <wakuthiichristine5@gmail.com>
2024-03-05 17:22:09 +0300

    salary search

--- Job-blog/src/Pages/SalaryPage.jsx
+++ Job-blog/src/Pages/SalaryPage.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import PageHeader from '../components/PageHeader'
 
+
 const SalaryPage = () => {
+    const[searchText, setSearchText] = useState("");
+    const [salary, setSalary] = useState([]);
+
+    useEffect( () => {
+        fetch("salary.json").then(res => res.json()).then(data => setSalary(data))
+    }, [])
+    const handleSearch = () => {
+        const filter = salary.filter(job => job.title.toLowerCase().indexOf(searchText.toLowerCase()) !== -1);
+        console.log(filter)
+        setSalary(filter);
+    
+    }
+    console.log(searchText)
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
         
-        <PageHeader/>
+        <PageHeader title={"Estimate Salary"} path={"Salary"}/>
+        <div className="mt-5">
+            <div className=" search-box p-2 text-center mb-2">
+                <input type="text" name="search" id="search" className="py-2 pl-3 border focus:outline-none lg:w-6/12 mb-4 w-full" onChange={e => setSearchText(e.target.value)}/>
+
+                <button onClick={handleSearch} className="py-2 px-8 mb-4 border focus:outline-none bg-blue rounded-sm text-white cursor-pointer font-semibold hover:bg-pink">Search</button> 
+
+            </div>
+
+        </div>
     </div>
   )
 }
--- Job-blog/src/components/PageHeader.jsx
+++ Job-blog/src/components/PageHeader.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const PageHeader = () => {
+const PageHeader = (title, path) => {
   return (
-    <div>PageHeader</div>
+    <div className="py-24 mt-3 rounded bg-[#FAFAFA] flex items-center justify-center">
+        <div>
+            <h2 className="text-3xl text-blue font-medium mb-1 text-center">Estimate Salary</h2>
+            <p className="text-sm text-center"><a href="/">Home</a> / Salary</p>
+        </div>
+    </div>
   )
 }
 
